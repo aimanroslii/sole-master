8e6e7030c196a8b976ff2a46e4d3a151abc2536e
Aiman Rosli <aimanrosli977@yahoo.com>
2024-11-24 22:12:53 +0800

    Fix error on page
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- app/(storefront)/bag/page.tsx
+++ app/(storefront)/bag/page.tsx
@@ -4,11 +4,12 @@ import { Cart } from "@/app/lib/interfaces";
 import { redis } from "@/app/lib/redis";
 import { Button } from "@/components/ui/button";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { ShoppingBagIcon } from "lucide-react";
+import { ShoppingBag } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { unstable_noStore as noStore } from "next/cache";
+
 import { redirect } from "next/navigation";
-import {unstable_noStore as noStore} from 'next/cache'
 
 export default async function BagRoute() {
   noStore();
@@ -29,14 +30,20 @@ export default async function BagRoute() {
 
   return (
     <div className="max-w-2xl mx-auto mt-10 min-h-[55vh]">
-      {!cart || !cart.items || cart.items.length === 0 ? (
-        <div className="flex min-h-[400px] flex-col items-center justify-center rounded-lg border-dashed p-8 text-center mt-20">
+      {!cart || !cart.items ? (
+        <div className="flex min-h-[400px] flex-col items-center justify-center rounded-lg border border-dashed p-8 text-center mt-20">
           <div className="flex h-20 w-20 items-center justify-center rounded-full bg-primary/10">
-            <ShoppingBagIcon className="w-10 h-10 text-primary" />
+            <ShoppingBag className="w-10 h-10 text-primary" />
           </div>
-          <h2 className="mt-6 mb-8 text-xl font-semibold">
-            Shopping Cart is Empty
+
+          <h2 className="mt-6 text-xl font-semibold">
+            You dont have any products in your Bag
           </h2>
+          <p className="mb-8 mt-2 text-center text-sm leading-6 text-muted-foreground max-w-sm mx-auto">
+            You currently dont have any products in your shopping bag. Please
+            add some so that you can see them right here.
+          </p>
+
           <Button asChild>
             <Link href="/">Shop Now!</Link>
           </Button>
@@ -45,12 +52,12 @@ export default async function BagRoute() {
         <div className="flex flex-col gap-y-10">
           {cart?.items.map((item) => (
             <div key={item.id} className="flex">
-              <div className="2-24 h-24 sm:w-32 sm:h-32 relative">
+              <div className="w-24 h-24 sm:w-32 sm:h-32 relative">
                 <Image
                   className="rounded-md object-cover"
                   fill
                   src={item.imageString}
-                  alt="Product Image"
+                  alt="Product image"
                 />
               </div>
               <div className="ml-5 flex justify-between w-full font-medium">
@@ -74,6 +81,7 @@ export default async function BagRoute() {
               <p>Subtotal:</p>
               <p>${new Intl.NumberFormat("en-US").format(totalPrice)}</p>
             </div>
+
             <form action={checkOut}>
               <CheckoutButton />
             </form>
--- app/(storefront)/page.tsx
+++ app/(storefront)/page.tsx
@@ -3,11 +3,11 @@ import { FeaturedProducts } from "../components/storefront/FeaturedProducts";
 import { Hero } from "../components/storefront/Hero";
 
 export default function IndexPage() {
-    return (
-        <div>
-            <Hero />
-            <CategoriesSelection />
-            <FeaturedProducts />
-        </div>
-    )
+  return (
+    <div>
+      <Hero />
+      <CategoriesSelection />
+      <FeaturedProducts />
+    </div>
+  );
 }
