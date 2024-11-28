d7547e0ab070afde9e1c685403cb1e3c4c0f0f1d
Aiman Rosli <aimanrosli977@yahoo.com>
2024-11-28 23:36:24 +0800

    update logo, header, title, and improve banner
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- app/components/storefront/Navbar.tsx
+++ app/components/storefront/Navbar.tsx
@@ -10,6 +10,7 @@ import {
 } from "@kinde-oss/kinde-auth-nextjs/components";
 import { redis } from "@/app/lib/redis";
 import { Cart } from "@/app/lib/interfaces";
+import Image from "next/image";
 
 export async function Navbar() {
   const { getUser } = getKindeServerSession();
@@ -23,9 +24,12 @@ export async function Navbar() {
     <nav className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-5 flex items-center justify-between">
       <div className="flex items-center">
         <Link href="/">
-          <h1 className="text-black font-bold text-xl lg:text-3xl">
-            Shoe<span className="text-primary">Marshal</span>
-          </h1>
+          <Image
+            src="/logo/soleMaster_whitebg.png"
+            alt="Logo Sole Master"
+            width={150}
+            height={40}
+          />
         </Link>
         <NavbarLinks />
       </div>
